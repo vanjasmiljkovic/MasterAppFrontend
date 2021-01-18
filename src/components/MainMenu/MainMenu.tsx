c6ceaf1496731d15a12e42c24f273ec755e17edc
Vanja Smiljkovic <vanjasmiljkovic22@gmail.com>
2021-01-18 15:05:27 +0100

    Add state to the main menu component

--- src/components/MainMenu/MainMenu.tsx
+++ src/components/MainMenu/MainMenu.tsx
@@ -15,12 +15,33 @@ interface MainMenuProperties {
     items: MainMenuItem[];
 }
 
+interface MainMenuState {
+    items: MainMenuItem[];
+}
+
 export class MainMenu extends React.Component<MainMenuProperties> {
+    state: MainMenuState;
+
+    constructor(props: Readonly<MainMenuProperties>) {
+        super(props);
+        //stanja
+        this.state = {
+            items: props.items, //to mu je pocetno stanje
+        };
+
+    }
+
+    setItems(items: MainMenuItem[]) { //ako neko pozove setItems i da neke druge item-e
+        this.setState({
+            items: items, //setovacemo umesto pocetnog stanja da dobije prosledjen items 
+        });
+    }
+
     render() {
         return (
             <Container>
                 <Nav variant="tabs">
-                    { this.props.items.map(this.makeNavLink) }
+                    { this.state.items.map(this.makeNavLink) }
                 </Nav>
             </Container>
         );
