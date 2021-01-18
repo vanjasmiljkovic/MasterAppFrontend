712c69a60ecb4481360c6eb0c8445fa78089fcc5
Vanja Smiljkovic <vanjasmiljkovic22@gmail.com>
2021-01-18 13:32:18 +0100

    Project cleanup

---
+++ src/components/App/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() { //funkcionalna komponenta - vraca ono sto treba da bude renderovano
+  return (
+    <div className="App">
+      Home
+    </div>
+  );
+}
+
+export default App;
